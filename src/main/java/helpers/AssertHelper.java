7aa7f0450dd249991ffaea4de10dbd2e4e95059d
Andrey Guschin <andrey67895@gmail.com>
2022-01-22 13:01:47 +0500

    Fixing Dependencies

--- src/main/java/helpers/AssertHelper.java
+++ src/main/java/helpers/AssertHelper.java
@@ -3,7 +3,7 @@ package helpers;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import lombok.extern.java.Log;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 @Log
 public class AssertHelper {
@@ -11,12 +11,13 @@ public class AssertHelper {
     @Step("Проверка ожидаемого и фактического значения")
     public static void assertEquals(Object expected, Object actual) {
         log.info(String.format("expected: %s and actual: %s", expected.toString(), actual.toString()));
-        Assert.assertEquals(String.format(" %s, is not equal to %s", expected, actual), expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Step("{msg}")
     public static void assertTrue(String msg, boolean condition) {
-        Assert.assertTrue(msg, condition);
+        log.info(msg);
+        Assertions.assertTrue(condition);
     }
 
     @Step("Проверка кода ответа")
